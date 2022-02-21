2e8db9d57deada7521e916c368aec5375637c68e
Daniel <danoladesign@gmail.com>
2022-02-20 23:29:33 -0600

    Clean up

--- routes/user.js
+++ routes/user.js
@@ -13,20 +13,4 @@ router.param('userId', userById)
 
 module.exports = router
 
-// const express = require('express')
-// const { userById, allUsers, getUser, updateUser, deleteUser } = require('../controllers/user')
-// const { requireSignin } = require('../controllers/auth')
 
-
-// const router = express.Router()
-
-// //gives logic to controller to handle 
-// router.get('/users', allUsers)
-// router.get('/user/:userId', requireSignin, getUser)
-// router.put('/user/:userId', requireSignin, updateUser) // put changes entire object
-// router.delete('/user/:userId', requireSignin, deleteUser) 
-
-// // if route contains :userId will execute userById method
-// router.param('userId', userById)
-
-// module.exports = router
